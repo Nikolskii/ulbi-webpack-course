05a7022c920da0c89962ce744c036d553fad24e0
Denis Nikolskii <den-apple@ya.ru>
2023-11-13 18:54:17 +0200

    Plugins, HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = (env) => {
   return {
@@ -8,6 +10,12 @@ module.exports = (env) => {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js',
       clean: true
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new webpack.ProgressPlugin()
+    ]
   };
 };
