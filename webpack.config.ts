4d3b3fc6f94218d8ba5594717e7dc769086402d7
Denis Nikolskii <den-apple@ya.ru>
2023-11-14 09:36:35 +0200

    Dev Server, Source Maps

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
-type Mode = 'development' | 'production'
+type Mode = 'development' | 'production';
 
 interface EnvVariables {
   mode: Mode;
+  port: number;
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development';
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -21,7 +25,7 @@ export default (env: EnvVariables) => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'public', 'index.html')
       }),
-      new webpack.ProgressPlugin()
+      isDev && new webpack.ProgressPlugin()
     ],
     module: {
       rules: [
@@ -34,7 +38,12 @@ export default (env: EnvVariables) => {
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
-    }
+    },
+    devtool: isDev && 'inline-source-map',
+    devServer: isDev ? {
+      port: env.port ?? 3000,
+      open: true
+    } : undefined
   };
 
   return config;
