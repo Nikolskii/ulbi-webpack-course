1dbe81d59ad9cf0eb15e9b410dd327bf6569c24f
Denis Nikolskii <den-apple@ya.ru>
2023-11-15 14:24:51 +0200

    Изоляция стилей, CSS modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,13 +6,22 @@ import { BuildOptions } from './types/types';
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
 
+  const cssLoaderWithModule = {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+      }
+    }
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // 'style-loader' creates `style` nodes from JS strings
       // MiniCssExtractPlugin extracts CSS into separate files
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      'css-loader', // Translates CSS into CommonJS
+      cssLoaderWithModule, // Translates CSS into CommonJS
       'sass-loader' // Compiles Sass to CSS
     ]
   };
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.scss';
+import classes from './App.module.scss';
 
 export const App = () => {
   const [count, setCount] = useState<number>(0);
@@ -8,8 +8,8 @@ export const App = () => {
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={increment}><span>inc</span></button>
+      <h1 className={classes.value}>{count}</h1>
+      <button className={classes.button} onClick={increment}><span>inc</span></button>
     </div>
   );
 };
